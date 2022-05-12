9864a758e098e545b08a555533edd08e5644f902
jack13only <ferretreva@gmail.com>
2022-05-12 13:53:20 +0300

    feat: add rtk query

---
+++ src/app/apiTypes.ts
@@ -0,0 +1,19 @@
+export type SigninType = {
+  login: string;
+  password: string;
+};
+
+export type SignupType = {
+  name: string;
+  login: string;
+  password: string;
+};
+
+export type BoardType = {
+  title: string;
+};
+
+export type ColumnType = {
+  title: string;
+  order: number;
+};
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { apiUser } from './RtkQuery';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [apiUser.reducerPath]: apiUser.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiUser.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
