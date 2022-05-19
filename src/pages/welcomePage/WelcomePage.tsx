9ae3eccf2d51e2b6a11a8765d851dea229e9fd75
Marharyta Malets <Marharyta-Malets@epam.com>
2022-05-19 16:56:18 +0300

    fix: welcome page

--- src/pages/welcomePage/WelcomePage.tsx
+++ src/pages/welcomePage/WelcomePage.tsx
@@ -4,13 +4,45 @@ import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { saveTokenToLS } from '../../features/ls-load-save';
 import { loginUser } from '../../reducers/auth';
 import { PATHS } from '../../shared/constants/routes';
+import { WelcomeCard } from './components/welcomeCard/WelcomeCard';
+import avatar1 from '../../images/avatar-1.jpg';
+import avatar2 from '../../images/avatar-2.jpg';
+import avatar3 from '../../images/avatar-3.jpg';
+import './WelcomePage.scss';
 
 const WelcomePage: FC = () => {
   const { userToken } = useAppSelector((state) => state.authStorage);
   const dispatch = useAppDispatch();
   return (
-    <div data-testid="welcomepage">
-      <div>Welcome Page</div>
+    <div data-testid="welcomepage" className="welcome__page">
+      <h2 className="h2">Meet Our Team</h2>
+      <div className="welcome__container">
+        <WelcomeCard
+            src={avatar2}
+            name="Yauheni Shatau"
+            specialization="Front-end Developer"
+            github="https://github.com/jack13only"
+            linkedin="https://www.linkedin.com/in/jack13only/"
+            instagram="https://www.instagram.com/"
+        />
+        <WelcomeCard
+            src={avatar3}
+            name="Victoria Kochieva"
+            specialization="Front-end Developer"
+            github="https://github.com/VictoriaKochieva"
+            linkedin="https://www.linkedin.com/in/victoria-kochieva/"
+            instagram="https://www.instagram.com/"
+        />
+        <WelcomeCard
+            src={avatar1}
+            name="Dzmitry Karakulka"
+            specialization="Front-end Developer"
+            github="https://github.com/mitrofanzxc"
+            linkedin="https://www.linkedin.com/in/dzmitry-karakulka/"
+            instagram="https://www.instagram.com/mitrofanzxc/"
+        />
+      </div>
+      <Link to="/boards">Link to Boards</Link>
       {userToken && (
         <>
           <p>
