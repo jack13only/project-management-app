50e494fc1f8044494b04c8a1154356065e5a197a
mitrofanzxc <mitrofanzxc@gmail.com>
2022-05-29 15:28:46 +0300

    refactor: ChangeTitleBtns styles (Sun, May 29, 2022  3:28:46 PM)

--- src/components/buttons/changeTitleBtns/ChangeTitleBtns.tsx
+++ src/components/buttons/changeTitleBtns/ChangeTitleBtns.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 
+import './ChangeTitleBtns.scss';
+
 interface ChangeTitleBtnsProps {
   onClickSubmit: (event: React.MouseEvent<HTMLButtonElement>) => void;
   onClickCancel: (event: React.MouseEvent<HTMLButtonElement>) => void;
@@ -8,12 +10,8 @@ interface ChangeTitleBtnsProps {
 const ChangeTitleBtns: FC<ChangeTitleBtnsProps> = ({ onClickSubmit, onClickCancel }) => {
   return (
     <div className="board__column-btns">
-      <button type="submit" onClick={onClickSubmit}>
-        Submit
-      </button>
-      <button type="button" onClick={onClickCancel}>
-        Cancel
-      </button>
+      <button className="button-modal button__submit" type="submit" onClick={onClickSubmit} />
+      <button className="button-modal button__cancel" type="button" onClick={onClickCancel} />
     </div>
   );
 };
